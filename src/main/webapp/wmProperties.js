d61bec966511590d7958fded19e522ac0fdb1eed
root <root@61949b3d2f95>
2017-07-06 01:36:33 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_BEkRZKRVLv",
+  "homePage" : "Main",
+  "name" : "Auto_BEkRZKRVLv",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
